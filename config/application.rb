3db49b606920c74bf8ef645e3c2c21c81f05cd36
Alex Somesan <alex.somesan@gmail.com>
2017-03-30 13:07:47 +0200

    Configure Prometheus client middleware

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require 
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -22,5 +23,15 @@ module ExampleStore
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Add the prometheus client middleware.
+    config.middleware.use Prometheus::Middleware::Exporter
+
+    config.after_initialize do
+	    $prometheus = Prometheus::Client.registry
+      rails_up = Prometheus::Client::Gauge.new(:app_running, 'Rails app has started')
+      $prometheus.register(rails_up)
+      rails_up.set({app: "Shoppe"}, 1)
+    end
   end
 end
